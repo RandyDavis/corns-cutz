2655310c8fcd841b6a75763dc520d0c1220872c1
Randy Davis <randy_davis@outlook.com>
2015-04-10 23:46:49 -0500

    update index.html page content and styles

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+var windowHeight = $(window).height();
+
+$(".contentContainer").css("min-height", windowHeight);
