9ed107000238651f04143fefdc3b5dab493756e6
Randy Davis <randy_davis@outlook.com>
2015-04-11 19:20:01 -0500

    Update index page content and styles

--- js/app.js
+++ js/app.js
@@ -5,3 +5,13 @@ $(document).foundation();
 var windowHeight = $(window).height();
 
 $(".contentContainer").css("min-height", windowHeight);
+
+
+// Date Script
+$(function() {
+  var dateNow = new Date();
+  var yearNow = dateNow.getFullYear();
+  var $footerDate = $('#footer');
+  $footerDate.append( "<p class='text-center'>&copy; " + yearNow + " Corn's Cutz - All Rights Reserved</p>");
+  
+});
